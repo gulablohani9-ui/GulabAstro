c686475ac939a186938ed02590889762af1151a0
gulablohani9-ui <gulablohani9@gmail.com>
2026-09-08 14:52:49 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,18 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io") // <-- Ye add karein
+    }
 }
+
 rootProject.name = "GulabAstro"
 include(":app")
